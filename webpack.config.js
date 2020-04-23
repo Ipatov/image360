0ade13d5a1a3ffbddfc113aa8410091e90f05a55
Alexey Rodin <razraz.odinodin@yandex.ru>
2020-04-23 14:23:20 +0300

    add webpack dev config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: path.join(__dirname, 'examples', 'src', 'index.html'),
+  filename: 'index.html',
+});
+
+
+module.exports = {
+  entry: path.join(__dirname, 'examples', 'src', 'index.js'),
+
+  output: {
+    path: path.join(__dirname, 'examples', 'dist'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ]
+  },
+
+  plugins: [htmlWebpackPlugin],
+
+  resolve: {
+    extensions: ['.js'],
+  },
+
+  devServer: {
+    port: 4000,
+  }
+};
